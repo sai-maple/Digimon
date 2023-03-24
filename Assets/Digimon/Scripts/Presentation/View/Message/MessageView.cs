d6c48317ef9f1d9c396e0fb34c8ad806245f13e0
hiroaki.goto <hiroakigotou1229@gmail.com>
2023-03-24 19:28:27 +0900

    メッセージスキップ修正

--- Assets/Digimon/Scripts/Presentation/View/Message/MessageView.cs
+++ Assets/Digimon/Scripts/Presentation/View/Message/MessageView.cs
@@ -9,49 +9,49 @@ namespace Digimon.Digimon.Scripts.Presentation.View.Message
 {
     public sealed class MessageView : MonoBehaviour
     {
-        [SerializeField] private RectTransform messageBox;
-        [SerializeField] private CanvasGroup canvasGroup;
-        [SerializeField] private TextMeshProUGUI message;
-        [SerializeField] private AudioSource audioSource;
-        [SerializeField] private Button nextButton;
-        [SerializeField] private Vector3 from;
-        [SerializeField] private Vector3 to;
+        [SerializeField] private RectTransform _messageBox;
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private TextMeshProUGUI _message;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private Button _nextButton;
+        [SerializeField] private Vector3 _from;
+        [SerializeField] private Vector3 _to;
 
         public void Initialize()
         {
-            messageBox.anchoredPosition = from;
-            message.text = "";
-            canvasGroup.interactable = false;
-            canvasGroup.blocksRaycasts = false;
+            _messageBox.anchoredPosition = _from;
+            _message.text = "";
+            _canvasGroup.interactable = false;
+            _canvasGroup.blocksRaycasts = false;
         }
 
         public async UniTask PresentAsync(CancellationToken token)
         {
             // todo シュって音出したい?
-            message.text = "";
-            await messageBox.DOAnchorPos(to, 0.5f).WithCancellation(token);
+            _message.text = "";
+            await _messageBox.DOAnchorPos(_to, 0.5f).WithCancellation(token);
             if (token.IsCancellationRequested) return;
-            canvasGroup.interactable = true;
-            canvasGroup.blocksRaycasts = true;
+            _canvasGroup.interactable = true;
+            _canvasGroup.blocksRaycasts = true;
         }
 
         public async UniTaskVoid DismissAsync(CancellationToken token)
         {
-            canvasGroup.interactable = false;
-            canvasGroup.blocksRaycasts = false;
-            await messageBox.DOAnchorPos(from, 0.5f).WithCancellation(token);
+            _canvasGroup.interactable = false;
+            _canvasGroup.blocksRaycasts = false;
+            await _messageBox.DOAnchorPos(_from, 0.5f).WithCancellation(token);
         }
 
         public async UniTask MessageAsync(string message, CancellationToken token)
         {
             if (string.IsNullOrEmpty(message)) return;
             var cancellation = new CancellationTokenSource();
-            audioSource.Play();
-            var buttonTask = nextButton.GetAsyncClickEventHandler();
-            var textTask = this.message.DOText(message, message.Length * 0.1f).WithCancellation(cancellation.Token);
+            _audioSource.Play();
+            var buttonTask = _nextButton.GetAsyncClickEventHandler();
+            var textTween = _message.DOText(message, message.Length * 0.1f);
 
             // ボタンクリックでメッセージ表示スキップ
-            var index = await UniTask.WhenAny(buttonTask.OnClickAsync(), textTask);
+            var index = await UniTask.WhenAny(buttonTask.OnClickAsync(), textTween.WithCancellation(cancellation.Token));
             // 呼び出し元が死んでたらreturn
             if (token.IsCancellationRequested) return;
             if (index == 0)
@@ -59,11 +59,12 @@ namespace Digimon.Digimon.Scripts.Presentation.View.Message
                 cancellation.Cancel();
                 cancellation.Dispose();
                 cancellation = null;
-                this.message.text = message;
+                textTween.Kill();
             }
 
+            _message.text = message;
             // カタカタ音停止 + もう一度タップで
-            audioSource.Stop();
+            _audioSource.Stop();
             await buttonTask.OnClickAsync();
             cancellation?.Cancel();
             cancellation?.Dispose();
